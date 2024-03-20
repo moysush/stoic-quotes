87e888ca9e5021b880bba0314ff3a62cb1873296
Sushmoy <80288978+SushCod3@users.noreply.github.com>
2024-03-20 09:04:07 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Stoic-Quotes/',
+  base: '/stoic-quotes/',
   plugins: [
     vue(),
   ],
